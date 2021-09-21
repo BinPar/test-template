f8a56ab3caea970f6f3784bdde51a0646b01ca66
Marcos González Andrés <marcos@binpar.com>
2021-09-21 11:37:11 +0200

    fix: script k8s and values

--- .github/workflows/create-k8s-if-not-exists.sh
+++ .github/workflows/create-k8s-if-not-exists.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 environments=$1
-../../k8s/templates/compile-templates.sh $environments
+cd ./k8s/templates
+./compile-templates.sh $environments
 for environment in $environments
 do
-  rancher kubectl apply -f ../../k8s/compiled/$environment.yml
+  rancher kubectl apply -f ../compiled/$environment.yml
 done
