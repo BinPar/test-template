6fb7687f4734f56da9756c75c993cab29c1136fe
Marcos González Andrés <marcos@binpar.com>
2021-09-21 09:28:03 +0200

    feat: hello binpar template

---
+++ pages/healthcheck.tsx
@@ -0,0 +1,10 @@
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  context.res.end('OK');
+  return {
+    props: {},
+  };
+};
+
+export default (): React.ReactNode => null;
---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+interface IndexProps {
+  title: string;
+}
+
+const index: React.FC<IndexProps> = ({ title }) => (
+  <React.Fragment>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <h1>{title}</h1>
+  </React.Fragment>
+);
+
+/**
+ * This is getServerSideProps to verify that the ENV is working,
+ * nota that this functionality (because of the static nature of the data)
+ * will fit better in the getStaticProps paradigm
+ */
+export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
+  return {
+    props: {
+      title: `Hello BinPar template!`,
+    },
+  };
+};
+
+export default index;
